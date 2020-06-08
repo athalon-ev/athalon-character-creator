0c3168c66f9b743ecea66c49bb70e19d8b438e9a
Martin Muzatko <martin@happy-css.com>
2020-06-09 00:06:11 +0200

    add descriptive texts

--- pages/index.vue
+++ pages/index.vue
@@ -6,22 +6,68 @@
             </h2>
             <v-form>
                 <v-text-field v-model="character.name" name="name" label="Name" />
-                <v-text-field v-model="character.age" name="age" type="number" :min="5" :max="200" label="Alter" />
-                <v-slider
-                    v-model="character.size"
-                    vertical name="height" label="Größe"
-                    :min="20" :max="250"
-                    thumb-label="always"
-                >
-                    <div slot="thumb-label">
-                        {{ character.size }}cm
+                <h2 class="text-xl font-bold">
+                    Äußerliche Erscheinung
+                </h2>
+                <p class="text-gray-700">
+                    Mit den Einstellungen kannst du dein Erscheinungsbild verfeinern, wie willst du auf andere wirken?
+                </p>
+                <div class="flex justify-between">
+                    <div class="w-1/2 pr-4">
+                        <v-text-field v-model="character.age" name="age" type="number" :min="5" :max="200" label="Alter" suffix="Jahre" />
+                        <v-slider
+                            v-model="character.size"
+                            vertical name="height" label="Größe"
+                            :min="20" :max="250"
+                            thumb-label="always"
+                        >
+                            <div slot="thumb-label">
+                                {{ character.size }}cm
+                            </div>
+                        </v-slider>
                     </div>
-                </v-slider>
-                <v-text-field v-model="character.weight" name="weight" type="number" :min="40" :max="150" label="Gewicht" />
-                <v-combobox name="haircolor" label="Haarfarbe" v-model="character.haircolor" :items="characterData.haircolors" />
-                <v-combobox name="eyecolor" label="Augenfarbe" v-model="character.eyecolor" :items="characterData.eyecolors" />
-                <v-combobox name="nationality" label="Nationalität" v-model="character.nationality" :items="nations" />
-                <v-select name="birthcity" label="Geburtsort" v-model="character.birthcity" :items="cities" />
+                    <div class="w-1/2">
+                        <v-combobox name="haircolor" label="Haarfarbe" v-model="character.haircolor" :items="characterData.haircolors" />
+                        <v-combobox name="eyecolor" label="Augenfarbe" v-model="character.eyecolor" :items="characterData.eyecolors" />
+                        <v-text-field v-model="character.weight" name="weight" type="number" :min="40" :max="150" label="Gewicht" suffix="kg" />
+                    </div>
+                </div>
+                <h2 class="text-xl font-bold">
+                    Herkunft
+                </h2>
+                <p class="text-gray-700">
+                    Die Herkunft bestimmt deinen Wortschatz und auch deine Religion und Kultur wie Bräuche
+                </p>
+                <div class="flex justify-between">
+                    <div class="w-1/2 pr-4">
+                        <v-select name="nationality" label="Nationalität" v-model="character.nationality" :items="nations" />
+                    </div>
+                    <div class="w-1/2">
+                        <v-combobox name="birthcity" label="Geburtsort" v-model="character.birthcity" :items="cities" />
+                    </div>
+                </div>
+                <v-textarea
+                    label="Äußerliche Erscheinung"
+                />
+                Äußerliche Erscheinung
+                Angewohnheiten
+                Weltanschauung
+                Ängste und Phobien
+                Profession vor Neu Corethon
+                Eltern
+                Andere Verwandte
+                Warum hast du die Heimat verlassen?
+                Was hast du zurück gelassen?
+                Was möchtest du erreichen?
+
+                Stärke
+                Konstitution
+                Geschick
+                Intelligenz
+                Geist
+                Trefferpunkte
+                Stabilität
+                Staturbonus
             </v-form>
             <v-btn color="primary" @click="randomize">
                 <v-icon class="mr-2">
