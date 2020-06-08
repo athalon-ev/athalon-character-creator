84ba7f8af669347b1b7dcb153400ee8ed6615485
Martin Muzatko <mmu@gps-stuttgart.de>
2020-06-08 15:45:15 +0200

    updated deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+    theme: {},
+    variants: {},
+    plugins: [],
+    purge: [
+        './pages/**/*.vue',
+    ],
 }
