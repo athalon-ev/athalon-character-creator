2e0eaba448b9f74fad4e9fa9c04af2d75b2f1393
Martin Muzatko <martin@happy-css.com>
2021-03-21 00:14:41 +0100

    updated eslint and configured ts eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,12 +1,37 @@
-export default {
-    "root": true,
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint"
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        'standard'
     ],
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
-    ]
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json']
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'standard'
+    ],
+    rules: {
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'quote-props': ['error', 'as-needed'],
+        indent: [
+            'warn',
+            4
+        ],
+        eqeqeq: 'off',
+        'comma-dangle': [
+            'off',
+            {
+                arrays: 'always-multiline',
+                objects: 'always-multiline',
+                functions: 'never',
+            },
+        ],
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
 }
