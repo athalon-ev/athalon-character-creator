d2b00bf618f98443576d121db6e6c6dcf992a059
Martin Muzatko <martin@happy-css.com>
2023-01-19 14:39:28 +0100

    register through forum api start

--- server/src/account/accountService.ts
+++ server/src/account/accountService.ts
@@ -38,6 +38,18 @@ export const login = async (dependencies: Dependencies, credentials: LoginCreden
     }
 }
 
+const register = () => {
+//     fetch("https://board.athalon.de/member.php", {
+//   "headers": {
+//     "content-type": "application/x-www-form-urlencoded",
+//   },
+//   "body": "regcheck1=&regcheck2=true&username=TestAccountBitteLoeschen3&password=asdasdasd&password2=asdasdasd&email=martin%2B232%40happy-css.com&email2=martin%2B232%40happy-css.com&referrername=&answer=fantastisches+rollenspiel&question_id=Y7FgQomQ4d6IQczo7cLLy4hpzNceOKOi&allownotices=1&receivepms=1&pmnotice=1&subscriptionmethod=0&timezoneoffset=1&dstcorrection=2&language=&regtime=1664274405&step=registration&action=do_register&regsubmit=Registrierung+best%C3%A4tigen%21",
+//   "method": "POST",
+//   "mode": "cors",
+//   "credentials": "include"
+// });
+}
+
 export const loginWithForumAccount = async (dependencies: Dependencies, credentials: LoginCredentials): Promise<ForumUser> => {
     const { data: result } = await dependencies.config.forumClient.post(addApiKey(dependencies, 'api.php'), credentials)
     return result
